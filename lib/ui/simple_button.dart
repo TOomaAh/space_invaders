c66c75c775480096a60ad15652d235c7cb12ed8a
TOomaAh <tbrunet72@gmail.com>
2022-09-24 18:01:12 +0200

    add pause en back button

---
+++ lib/ui/simple_button.dart
@@ -0,0 +1,65 @@
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flame/experimental.dart';
+import 'package:flame/game.dart';
+import 'package:flame/rendering.dart';
+import 'package:flutter/rendering.dart';
+import 'package:fyc/game/invaders.dart';
+
+abstract class SimpleButton extends PositionComponent with TapCallbacks {
+  SimpleButton(this._iconPath, {super.position}) : super(size: Vector2.all(40));
+
+  final Paint _borderPaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..color = const Color(0x66ffffff);
+  final Paint _iconPaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..color = const Color(0xffaaaaaa)
+    ..strokeWidth = 7;
+  final Path _iconPath;
+
+  void action();
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(size.toRect(), const Radius.circular(8)),
+      _borderPaint,
+    );
+    canvas.drawPath(_iconPath, _iconPaint);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    _iconPaint.color = const Color(0xffffffff);
+  }
+
+  @override
+  void onTapUp(TapUpEvent event) {
+    _iconPaint.color = const Color(0xffaaaaaa);
+    action();
+  }
+
+  @override
+  void onTapCancel(TapCancelEvent event) {
+    _iconPaint.color = const Color(0xffaaaaaa);
+  }
+}
+
+/// SimpleButton is a PositionComponent
+class BackButton extends SimpleButton with HasGameRef<InvadersGame> {
+  /// BackButton is a SimpleButton
+  BackButton()
+      : super(
+          Path()
+            ..moveTo(22, 8)
+            ..lineTo(10, 20)
+            ..lineTo(22, 32)
+            ..moveTo(12, 20)
+            ..lineTo(34, 20),
+          position: Vector2.all(10),
+        );
+
+  @override
+  void action() => gameRef.router.pop();
+}
